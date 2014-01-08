356bc38304f5b9e8f623eda84f0007ce36066f0a
PhilipGarnero <philip.garnero@epitech.eu>
2014-01-08 19:04:23 +0100

    ajout de user et save auto

--- Header.py
+++ Header.py
@@ -14,7 +14,6 @@ class PromptHeaderCommand(sublime_plugin.WindowCommand):
   def run(self):
     label = "Type project name: "
     self.window.show_input_panel(label, "", self.on_done, None, None)
-    pass
 
   def on_done(self, text):
     try:
@@ -23,21 +22,34 @@ class PromptHeaderCommand(sublime_plugin.WindowCommand):
       pass
 
 
-#
-# Main class: create the epitech-style header.
-#
+class HeaderUpdater(sublime_plugin.EventListener):
+    def on_pre_save(self, view):
+        try:
+          edit = view.begin_edit()
+          header = Header(view)
+          new_line = header.get_comment()[1] + " Last update " + header.get_date() + " " + header.fullname
+          region = view.find("Last update", 0)
+          line = view.line(region)
+          view.replace(edit, line, new_line)
+        except Exception as e:
+          print e
+        finally:
+          view.end_edit(edit)
 
-class HeaderCommand(sublime_plugin.TextCommand):
 
+class Header():
   #
   # /!\ Find how to get this from system!
   #
-
-  fullname = "jeremie taboada-alvarez"
+  result = os.popen("getent passwd `whoami` | cut -d ':' -f 5 | cut -d ',' -f 1")
+  fullname = result.read()[:-1]
 
   #
   # Get comment type according language.
   #
+
+  def __init__(self, view):
+    self.view = view
 
   def get_comment(self):
     comments = {}
@@ -68,7 +80,7 @@ class HeaderCommand(sublime_plugin.TextCommand):
 
   def get_file_infos(self):
     full = self.view.file_name().split('/')
-    return [full.pop(), '/'.join(full)]    
+    return [full.pop(), '/'.join(full)]
 
   #
   # Get email
@@ -89,28 +101,32 @@ class HeaderCommand(sublime_plugin.TextCommand):
 
     return time.strftime("%a %b  %d %H:%M:%S %Y")
 
+#
+# Main class: create the epitech-style header.
+#
+class HeaderCommand(sublime_plugin.TextCommand):
   #
   # Generate header.
   #
-
   def generate(self, project):
 
     # get some infos
 
+    helper = Header(self.view)
     header = ""
-    comment = self.get_comment()
-    f = self.get_file_infos()
+    comment = helper.get_comment()
+    f = helper.get_file_infos()
 
     # generate the header
 
     header += comment[0] + '\n'
     header += comment[1] + " " + f[0] + " for " + project + " in " + f[1] + '\n'
     header += comment[1] + '\n'
-    header += comment[1] + " Made by " + self.fullname + '\n'
-    header += comment[1] + " Login   " + self.get_mail() + '\n'
+    header += comment[1] + " Made by " + helper.fullname + '\n'
+    header += comment[1] + " Login   " + helper.get_mail() + '\n'
     header += comment[1] + '\n'
-    header += comment[1] + " Started on  " + self.get_date() + " " + self.fullname + '\n'
-    header += comment[1] + " Last update " + self.get_date() + " " + self.fullname + '\n'
+    header += comment[1] + " Started on  " + helper.get_date() + " " + helper.fullname + '\n'
+    header += comment[1] + " Last update " + helper.get_date() + " " + helper.fullname + '\n'
     header += comment[2] + '\n'
 
     return header
